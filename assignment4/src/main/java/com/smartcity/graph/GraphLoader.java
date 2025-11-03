3fa45493bd52201111d0cd26f4d498a95afb34c0
petrichorrr006 <raven24112006@gmail.com>
2025-11-03 10:12:16 +0500

    add graph loader

--- assignment4/src/main/java/com/smartcity/graph/GraphLoader.java
+++ assignment4/src/main/java/com/smartcity/graph/GraphLoader.java
@@ -8,20 +8,32 @@ public class GraphLoader {
 
     public static Map<Integer, List<int[]>> loadWeighted(String path) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
+
         try (Reader reader = new FileReader(path)) {
             JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
+
+            JsonArray nodes = json.getAsJsonArray("nodes");
+            for (JsonElement n : nodes) {
+                int node = n.getAsInt();
+                graph.putIfAbsent(node, new ArrayList<>());
+            }
+
             JsonArray edges = json.getAsJsonArray("edges");
             for (JsonElement e : edges) {
                 JsonObject o = e.getAsJsonObject();
                 int from = o.get("from").getAsInt();
                 int to = o.get("to").getAsInt();
-                int w = o.get("weight").getAsInt();
+                int w = o.has("weight") ? o.get("weight").getAsInt() : 1;
+
                 graph.putIfAbsent(from, new ArrayList<>());
+                graph.putIfAbsent(to, new ArrayList<>());
                 graph.get(from).add(new int[]{to, w});
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return graph;
     }
 
